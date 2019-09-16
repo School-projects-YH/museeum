f80798c199122d7e68445305477ab1611d8c6550
Lukas <lukas.selin@viskan.se>
2019-09-16 18:23:30 +0200

    3rd entry some cases are working still need alot of work

--- PrintRoom.cs
+++ PrintRoom.cs
@@ -18,7 +18,7 @@ namespace museumet_1976
             for (int i = 0; i <= numbeOfRooms; i++){
                 int length = rand.Next(5, 12);
                 int height = rand.Next(2, 5);
-				int startX = rand.Next(0, length);
+				int startX = rand.Next(0, length - 2);
 
                 PrintOneRoom(length, height, oldX, startX, oldLength);
 
@@ -38,28 +38,23 @@ namespace museumet_1976
         }
         static void PrintRow(int length, int startX, int oldX, int oldLength)
         {
-            if (startX < oldX ){
-                for (int y = 0; y < startX; y++ ){
-                    Console.Write(y);
-                }   
+            if (startX < oldX){
+        		PrintFiller(startX, ' ');   
                 Console.Write('G');
-
-                for (int i = 0; i < (oldX - startX -1); i++){
-                    Console.Write('-');
-                }
+				
+				// First part of the Row
+				int firstDiff = oldX - startX -1; 
+				PrintFiller(firstDiff, '-');				
                	Console.Write('+');
 
-				if ((length + startX) < (oldLength + oldX)){
-					for (int i = 0; i < (length - (oldX - startX)); i++){
-						Console.Write('-');
-					}
-					Console.Write("Hej");
-					
+				// Second part of the row. After the second room wall
+				int secondDiff = length - firstDiff - 1;
+				if (oldLength < length){
+					PrintFiller(oldLength, '-');
+				}else{
+					PrintFiller(secondDiff, '-');
 				}
-
-
-				
-
+				Console.Write("+");
 
             }else{
                 for (int y = 0; y < startX; y++ ){
@@ -67,12 +62,27 @@ namespace museumet_1976
                 }   
                     Console.Write('+');
 
-                for (int i = 0; i < length; i++){
-                    Console.Write('-');
-                }
+				PrintFiller(length, '-');
                 Console.Write('+');
             }
         }
+		static bool isPreviousRoomBigger(int startX, int length, int oldX, int oldLength)
+		{
+			int currentRoom = startX + length;
+			int previousRoom = oldX + oldLength;
+
+			if (currentRoom < previousRoom){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		static void PrintFiller(int amountOfTimes, char filler)
+		{
+  			for (int i = 0; i < amountOfTimes; i++){
+            	Console.Write(filler);
+            }
+		}
         static void PrintContent(int length, int height, int startX)
         {
             for (int i = 0; i <= height; i++){
